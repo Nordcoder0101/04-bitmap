8667a263b1a5859f6acd4f30df1a3521c35d78c9
Your Name <nords3x4@gmail.com>
2018-02-18 23:01:22 -0800

    final touches

--- lab-greg-nicole-ahmed/index.js
+++ lab-greg-nicole-ahmed/index.js
@@ -2,13 +2,13 @@
 
 
 const fileReader = require('./lib/read-write-file.js');
-const allwhite = require('./lib/all-black.js');
-const blueStripe = require('./lib/blue-stripe.js');
+const crosses = require('./lib/crosses.js');
 const darken = require('./lib/darken.js');
-const lighten = require('./lib/lighten.js');
-const random = require('./lib/neww.js');
+// const lighten = require('./lib/lighten.js');
+const random = require('./lib/randomPixels.js');
 
 fileReader('palette-bitmap.bmp', 'darkened.bmp', darken);
-fileReader('palette-bitmap.bmp', 'lightened.bmp', lighten);
-fileReader('palette-bitmap.bmp', 'neww.bmp', neww);
+// fileReader('palette-bitmap.bmp', 'lightened.bmp', lighten);
+fileReader('palette-bitmap.bmp', 'random.bmp', random);
+fileReader('palette-bitmap.bmp', 'crosses.bmp', crosses);
 
